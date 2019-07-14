1f656d8db04136e3b68956228cdf0a5bf76c3bcb
CI-Leo.Chuang <leo.chuang@cloud-interactive.com>
2019-07-15 03:07:25 +0800

    Updates

---
+++ precache-manifest.c65cb375763460709416b329423f66d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216c0272385550ba6b3b66435274f0f0",
+    "url": "/POMODORO/index.html"
+  },
+  {
+    "revision": "fa1b358cda1d49decc93",
+    "url": "/POMODORO/static/css/2.c59c7fe7.chunk.css"
+  },
+  {
+    "revision": "4ddaa407495f5f24494f",
+    "url": "/POMODORO/static/css/main.f7f0252b.chunk.css"
+  },
+  {
+    "revision": "fa1b358cda1d49decc93",
+    "url": "/POMODORO/static/js/2.314145ec.chunk.js"
+  },
+  {
+    "revision": "4ddaa407495f5f24494f",
+    "url": "/POMODORO/static/js/main.ae173808.chunk.js"
+  },
+  {
+    "revision": "53e407f664cbfc0df8da",
+    "url": "/POMODORO/static/js/runtime~main.1caec113.js"
+  },
+  {
+    "revision": "61cdd365b7242e884141f908590ba210",
+    "url": "/POMODORO/static/media/BebasNeue-Bold.61cdd365.woff"
+  },
+  {
+    "revision": "52299a3ba81a66a6060ba9037557aa51",
+    "url": "/POMODORO/static/media/BebasNeue-Book.52299a3b.woff"
+  },
+  {
+    "revision": "54bd57d957e337bcaa2dd2490b14ceb3",
+    "url": "/POMODORO/static/media/BebasNeue-Light.54bd57d9.woff"
+  },
+  {
+    "revision": "b6820090f2aa8b898f42b709b17fbb6a",
+    "url": "/POMODORO/static/media/BebasNeue-Regular.b6820090.woff"
+  },
+  {
+    "revision": "8eb07671d3ddd3f20f6e5ff86e97bb2b",
+    "url": "/POMODORO/static/media/BebasNeue-Thin.8eb07671.woff"
+  }
+]);
